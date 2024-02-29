ddf4672dd6f569e29c319c50ffb1d309dd17f50b
Shokhrukhbek Yuldoshev <72590392+ShokhrukhbekYuldoshev@users.noreply.github.com>
2024-03-01 01:21:35 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,50 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
- 
+
 <script>
 export default {
-  name: 'App',
-  components: {
-
-  }
+    name: 'App',
+    components: {},
 };
 </script>
 
-
 <style>
 body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  background-color: #f4f4f4;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
 }
 
 #app {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
 }
 
 h1 {
-  text-align: center;
-  color: #333;
+    text-align: center;
+    color: #333;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 
 button {
-  padding: 5px 10px;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: #007bff;
-}
-
-/* on hover darkens the button */
-button:hover {
-  background-color: darken;
+    padding: 5px 10px;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: #007bff;
 }
 </style>
